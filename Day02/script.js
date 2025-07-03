f1afe9523530e60890f00b483956a2fa55be8484
todolist <ushakanadi123@gmail.com>
2025-07-03 18:08:39 +0530

    Updated DummyWebapp code

--- Day02/script.js
+++ Day02/script.js
@@ -1,7 +1,12 @@
-$('#skillsCarousel').on('slide.bs.carousel', function () {
-  // Animate all progress bars when the slide changes
-    $('.progress-bar').each(function () {
-    let value = $(this).attr('style').match(/\d+/)[0]; // get percentage number
-    $(this).css('width', '0%').animate({ width: value + '%' }, 1000);
-    });
+function toggleSkill(element) {
+  element.classList.toggle("active-skill");
+}
+
+
+$(function (){
+  $('#contactForm').on('submit', function (e) {
+    e.preventDefault();
+    alert("Thankyou for messageing me. I will get back to you soon.");
+    $(this).trigger("reset");
+  });
 });
